58fb33e8dc71fd82a2099bea5503e987d1fbf737
InHoYi <129274888+InHoYi@users.noreply.github.com>
2024-05-07 22:07:44 +0900

    function added
    add get_dataframe

--- ProcessDataset.py
+++ ProcessDataset.py
@@ -1,62 +1,55 @@
 import pandas as pd
 import numpy as np
 
-
 class LoadDataset:
 
-    def __init__(self, window_size=0, numberOfFeatures=0):
+    def __init__(self, filepath, window_size = None, numberOfFeatures = 2):
+        self.filepath = filepath
         self.window_size = window_size
         self.numberOfFeatures = numberOfFeatures
+        self.data = self.load_data()
+        self.data = self.extract_data()
+        self.data = self.scale_data()
 
-    def setWindowSize(self, inputNumber):
-        self.window_size = inputNumber
+    def load_data(self):
+        data = pd.read_csv(self.filepath)
+        data['measure_dtm'] = pd.to_datetime(data['measure_dtm'])
+        data.replace({"OFF": 0, "ON": 1}, inplace=True)
+        return data
 
-    def setNumberOfFeatures(self, inputNumber):
-        self.numberOfFeatures = inputNumber
+    def get_dataframe(self):
+        result = pd.DataFrame(self.data)
+        return result
 
+    def scale_data(self):
+        scaled_data = self.data.copy()
+        min_val = np.min(scaled_data['attribute_1_value'])
+        max_val = np.max(scaled_data['attribute_1_value'])
+        scaled_data['attribute_1_value'] = (scaled_data['attribute_1_value'] - min_val) / (max_val - min_val)
+        return scaled_data
 
-    def createDataset(self, dataset, shuffle = None):
-        X = []
-        y = []
-        for i in range(len(dataset) - self.window_size):
-            X.append(dataset[i:(i + self.window_size)])
-            y.append(dataset[i + self.window_size][0])
-        
-        X = np.array(X)
-        y = np.array(y)
-        if (shuffle == True):
+    def extract_data(self):
+        extracted_data = self.data[['attribute_1_value', 'attribute_2_value']]
+        return extracted_data
+
+    def train_test_split(self, set_train_test_split_ratio = 0.8, set_train_valid_split_ratio = 0.75):
+        trainTestSplitLength = int(len(self.data) * set_train_test_split_ratio)
+        trainValidSplitLength = int(trainTestSplitLength * set_train_valid_split_ratio)
+        train = self.data.iloc[:trainValidSplitLength]
+        valid = self.data.iloc[trainValidSplitLength:trainTestSplitLength]
+        test = self.data.iloc[trainTestSplitLength:]
+        return train, valid, test 
+
+    def create_dataset_for_timeseires(self, shuffle = None):
+        X, y = [], []
+
+        for i in range(len(self.data) - self.window_size):
+            X.append(self.data.iloc[i: i + self.window_size].values)
+            y.append(self.data.iloc[i + self.window_size, 0])
+        X, y = np.array(X), np.array(y)
+
+        if shuffle:
             indices = np.random.permutation(len(X))
-            X_shuffled = X[indices]
-            y_shuffled = y[indices]
-        
-            return X_shuffled, y_shuffled
-        
-        else:
-            return X, y
+            X, y = X[indices], y[indices]
 
-    ##############################################################################################
-
-    def load_trainData(self, shuffle = None):
-        train_X, train_Y = self.createDataset(self.train, self.window_size, shuffle = shuffle)
-        train_X = train_X.reshape(train_X.shape[0], train_X.shape[1], self.numberOfFeatures)
-        return train_X, train_Y
-
-    def load_validData(self, shuffle = None):
-        valid_X, valid_Y = self.createDataset(self.valid, self.window_size, shuffle = shuffle)
-        return valid_X, valid_Y
-
-    def load_testData(self, shuffle = None):
-        test_X, test_Y = self.createDataset(self.test, self.window_size, shuffle = shuffle)
-        return test_X, test_Y
-
-    ##############################################################################################
-    
-    def load_OriginalData(self, shuffle = None):
-        Original_X, _ = self.createDataset(self.datasample, self.window_size, shuffle = shuffle)
-        Original_X = Original_X.reshape(Original_X.shape[0], Original_X.shape[1], 2)
-        return Original_X, _
-    
-    def load_DoorOpenData(self, shuffle = None):
-        Opened_X, _ = self.createDataset(self.OpenedSample, self.window_size, shuffle = shuffle)
-        Opened_X = Opened_X.reshape(Opened_X.shape[0], Opened_X.shape[1], 2)
-        return Opened_X, _
+        return X.reshape(-1, self.window_size, self.numberOfFeatures), y
