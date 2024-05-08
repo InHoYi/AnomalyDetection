dfbedb617d9607853b7285ca237f1e67acaa2253
InHoYi <129274888+InHoYi@users.noreply.github.com>
2024-05-09 00:12:48 +0900

    Updated file
    New model, updated loss, metrics.
    New way of validation.

--- Model/LSTMAE_KERAS.py
+++ Model/LSTMAE_KERAS.py
@@ -4,54 +4,36 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.callbacks import EarlyStopping
 
-number_of_features = 2
+number_of_features = 1
 
 seed = 42
 np.random.seed(seed)       
 random.seed(seed)          
 tf.random.set_seed(seed)
 
-def get_model(dataset):
+
+def get_model(window_size, number_of_features):
     model = keras.Sequential()
 
-    model.add(keras.layers.LSTM(64, input_shape = (dataset.shape[1], dataset.shape[2]), activation='relu', return_sequences = True))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dropout(0.2))
+    # Encoder
+    model.add(keras.layers.LSTM(64, input_shape = (window_size, number_of_features), activation='relu', return_sequences=True))
+    model.add(keras.layers.LSTM(32, return_sequences=False))
+    model.add(keras.layers.Dense(16, activation='relu'))
 
-    model.add(keras.layers.LSTM(32, return_sequences = True))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dropout(0.2))
-
-    model.add(keras.layers.LSTM(16, return_sequences = False))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dropout(0.2))
-
-    model.add(keras.layers.Dense(8))
-    model.add(keras.layers.Dropout(0.2))
-
-    model.add(keras.layers.RepeatVector(dataset.shape[1]))
-
-    model.add(keras.layers.LSTM(16, return_sequences = True))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dropout(0.2))
-
-    model.add(keras.layers.LSTM(32, return_sequences = True))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dropout(0.2))
-
-    model.add(keras.layers.LSTM(64, return_sequences = True))
-    model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dropout(0.2))
-
-    model.add(keras.layers.TimeDistributed(keras.layers.Dense(dataset.shape[2])))
+    # Decoder
+    model.add(keras.layers.RepeatVector(window_size)) 
+    model.add(keras.layers.LSTM(32, return_sequences=True))
+    model.add(keras.layers.LSTM(64, return_sequences=True))
+    model.add(keras.layers.TimeDistributed(keras.layers.Dense(number_of_features)))
 
     return model
 
 
 def train(model, train_X, valid_X, set_epochs = None):
-    model.compile(optimizer = 'adam', loss = 'mse', metrics = ['mse'])
-    earlyStopping = EarlyStopping( monitor = 'val_loss', patience = 20, verbose = 1, restore_best_weights = True)
-    history = model.fit(train_X, train_X, epochs = set_epochs, batch_size = 16, validation_data = (valid_X, valid_X), callbacks = [earlyStopping])
+    model.compile(optimizer = 'adam', loss = 'mae', metrics = ['mae'])
+    earlyStopping = EarlyStopping( monitor = 'val_loss', patience = 50, verbose = 1, restore_best_weights = True)
+    # history = model.fit(train_X, train_X, epochs = set_epochs, batch_size = 32, validation_data = (valid_X, valid_X), callbacks = [earlyStopping])
+    history = model.fit(train_X, train_X, epochs = set_epochs, batch_size = 32, validation_split = 0.1, callbacks = [earlyStopping])
     return history
 
 def print_summary(model):
